ad5f863b394f0907b864d703a275e41725e4b13a
Samuel Huang <hi@sghuang.com>
2025-02-02 17:17:26 -0500

    feat: add support for mdi icons (#2)
    * Add default icon sets
    
    * Install mdi fonts
    
    * Import icons css

--- setup/main.ts
+++ setup/main.ts
@@ -4,6 +4,7 @@ import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+import "@mdi/font/css/materialdesignicons.css";
 
 const vuetify = createVuetify({
   components,
@@ -16,9 +17,12 @@ export default defineAppSetup(({ app }) => {
     const isDark = document.documentElement.classList.contains("dark");
     vuetify.theme.global.name.value = isDark ? "dark" : "light";
     console.debug("[Veutify] Theme updated to:", isDark ? "dark" : "light");
-    console.debug("[Veutfiy] Theme object:", vuetify.theme)
+    console.debug("[Veutfiy] Theme object:", vuetify.theme);
   });
   // Observe class attribute changes on <html>
-  observer.observe(document.documentElement, { attributes: true, attributeFilter: ["class"] });
+  observer.observe(document.documentElement, {
+    attributes: true,
+    attributeFilter: ["class"],
+  });
   console.debug("[Vuetify] Theme observer started");
 });
