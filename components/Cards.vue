b153f24e0b0db277129ded6e2e121ea48884c603
Samuel Huang <hi@sghuang.com>
2025-02-02 16:53:08 -0500

    Cards demo

---
+++ components/Cards.vue
@@ -0,0 +1,49 @@
+<template>
+  <!-- Combined -->
+
+  <v-card
+    class="mx-auto mb-2rem"
+    prepend-icon="$vuetify"
+    subtitle="The #1 Vue UI Library"
+    width="400"
+  >
+    <template v-slot:title>
+      <span class="font-weight-black">Welcome to Vuetify</span>
+    </template>
+
+    <v-card-text class="bg-surface-light pt-4">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione
+      debitis quis est labore voluptatibus! Eaque cupiditate minima, at placeat
+      totam, magni doloremque veniam neque porro libero rerum unde voluptatem!
+    </v-card-text>
+  </v-card>
+
+  <!-- Variants -->
+
+  <v-row dense>
+    <v-col v-for="(variant, i) in variants" :key="i" cols="2" md="4">
+      <v-card
+        :variant="variant"
+        class="mx-auto"
+        color="surface-variant"
+        max-width="344"
+        subtitle="Greyhound divisely hello coldly fonwderfully"
+        title="Headline"
+      >
+        <template v-slot:actions>
+          <v-btn text="Button"></v-btn>
+        </template>
+      </v-card>
+
+      <div class="text-center text-caption">{{ variant }}</div>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    variants: ["elevated", "flat", "tonal", "outlined", "text", "plain"],
+  }),
+};
+</script>
--- slides.md
+++ slides.md
@@ -5,3 +5,9 @@
 ## Buttons
 
 <Buttons/>
+
+---
+
+## Cards
+
+<Cards />
