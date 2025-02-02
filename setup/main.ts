9d5f1a84c025c27593b4f5a807c6feb13837c4f8
Samuel Huang <hi@sghuang.com>
2025-02-02 18:59:23 -0500

    Listen to the global __slidev__ object

--- setup/main.ts
+++ setup/main.ts
@@ -1,3 +1,4 @@
+import { nextTick, onMounted } from "vue";
 import { defineAppSetup } from "@slidev/types";
 
 // import "vuetify/styles"; // seems like this is not needed
@@ -10,6 +11,30 @@ const vuetify = createVuetify({
   components,
   directives,
 });
+
+const checkSlidev = (app) => {
+  // __slidev__ is not available immediately
+  // TS doesn't know about __slidev__
+  const typedWindow = window as typeof window & { __slidev__?: any };
+  const observer = new MutationObserver(() => {
+    if (typedWindow) {
+      console.debug(
+        "[Vuetify] __slidev__ detected via MutationObserver:",
+        typedWindow.__slidev__,
+      );
+      observer.disconnect(); // Stop observing once detected
+    }
+  });
+
+  // watch for change in the DOM
+  observer.observe(document.documentElement, {
+    attributes: true,
+    childList: true,
+    subtree: true,
+  });
+
+  console.debug("[Vuetify] Waiting for __slidev__ via MutationObserver...");
+};
 
 const checkTheme = () => {
   const observer = new MutationObserver(() => {
@@ -28,5 +53,6 @@ const checkTheme = () => {
 
 export default defineAppSetup(({ app }) => {
   app.use(vuetify);
+  checkSlidev(app);
   checkTheme();
 });
