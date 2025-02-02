51f622594dddc3d9fbe149ff7ab6351ca44387cb
Samuel Huang <hi@sghuang.com>
2025-02-02 17:10:37 -0500

    Add default icon sets

--- setup/main.ts
+++ setup/main.ts
@@ -4,10 +4,18 @@ import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
 
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
 });
 
 export default defineAppSetup(({ app }) => {
@@ -16,9 +24,12 @@ export default defineAppSetup(({ app }) => {
     const isDark = document.documentElement.classList.contains("dark");
     vuetify.theme.global.name.value = isDark ? "dark" : "light";
     console.debug("[Veutify] Theme updated to:", isDark ? "dark" : "light");
-    console.debug("[Veutfiy] Theme object:", vuetify.theme)
+    console.debug("[Veutfiy] Theme object:", vuetify.theme);
   });
   // Observe class attribute changes on <html>
-  observer.observe(document.documentElement, { attributes: true, attributeFilter: ["class"] });
+  observer.observe(document.documentElement, {
+    attributes: true,
+    attributeFilter: ["class"],
+  });
   console.debug("[Vuetify] Theme observer started");
 });
